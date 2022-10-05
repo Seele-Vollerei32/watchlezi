10f5abd64c5799e3b151134fa4715d3d1e7e9cdf
kevin-liu15 <we123445@outlook.com>
2022-10-05 18:32:02 +0800

    update theme for better look

--- node_modules/camaro/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare const camaro: {
-    prettyPrint(xml: string, opts?: { indentSize: number }): Promise<string>;
-    toJson(xml: string): Promise<any>;
-    transform(xml: string, template: object): Promise<any>;
-};
-
-export = camaro;
--- node_modules/camaro/index.mjs
+++
@@ -1,6 +0,0 @@
-import mod from "./index.js";
-
-export default mod;
-export const transform = mod.transform;
-export const toJson = mod.toJson;
-export const prettyPrint = mod.prettyPrint;
